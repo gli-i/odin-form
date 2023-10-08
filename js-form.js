837d06c83060a37b9eafffa9e0df59310fcbaab4
Grace Li <133687652+gli-i@users.noreply.github.com>
2023-10-07 22:20:39 -0700

    Only allow form submission if all inputs valid

--- js-form.js
+++ js-form.js
@@ -17,14 +17,21 @@ for (let i = 0; i<text_inputs.length; i++){
     })
 }
 
-// if the password changes & there is input in confirm pw, recheck confirm pw's validity
-pw_input.addEventListener('input', () => {
-    if (conpw_input.value != ""){
-        validity = checkError_conpw(conpw_input);
-        showError(validity, conpw_input, conpw_input.parentNode.nextElementSibling);
+// check for validity when the page is first loaded
+window.addEventListener('load', () => {
+    for (let i = 0; i<text_inputs.length; i++){
+        let validity = checkError[i](text_inputs[i]);
+        showError(validity, text_inputs[i], error_msgs[i]);
     }
 })
 
+// if the password changes & there is input in confirm pw, recheck confirm pw's validity
+pw_input.addEventListener('input', () => {
+    validity = checkError_conpw(conpw_input);
+    showError(validity, conpw_input, conpw_input.parentNode.nextElementSibling);
+})
+
+/* TEXT VALIDATION FUNCTIONS */
 
 // First Name
 // req: not empty
@@ -47,7 +54,7 @@ function checkError_pw(elem) {
     if (!input) return "empty";
     if (input.length < 8) return "shortpw";
 
-    const pwRegExp = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])$/;
+    const pwRegExp = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
     return (pwRegExp.test(input)) ? "valid" : "weakpw";
 }
 // Last Name
@@ -61,7 +68,7 @@ function checkError_pnum(elem) {
     const input = elem.value;
     if (!input) return "valid";
 
-    const pnumRegExp = /^[+]?[\s./0-9]*[(]?[0-9]{1,4}[)]?[-\s./0-9]*$/g;
+    const pnumRegExp = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
     return (pnumRegExp.test(input)) ? "valid" : "notpnum";
 }
 // Confirm Password
@@ -108,6 +115,30 @@ function showError(validity, elem, msg_elem){
             msg_elem.textContent = "Does not match password";
             break;
     }
-    elem.className = "invalid";
+    elem.className = "invalid"; // change css styling of the input
     return;
+}
+
+/* BUTTON SUBMISSION */
+
+const form = document.querySelector('form');
+
+// prevent the form from submitting unless no inputs are invalid
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    if (checkValidity()){
+        form.submit();
+    }
+})
+
+// check if each input is valid
+function checkValidity() {
+    let ret = true;
+    text_inputs.forEach(e => {
+        if (e.className === "invalid"){
+            console.log(e);
+            ret = false;
+        }
+    })
+    return ret;
 }
